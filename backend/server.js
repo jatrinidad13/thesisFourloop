814f0e8c62200282931d1da5f0803567bf9f7651
Juan Antonio <jatrinidad0613@gmail.com>
2024-11-12 19:20:43 +0800

    update backend

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Allow CORS from frontend
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://fourloop.netlify.app/'],
+  origin: ['http://localhost:5173', 'https://fourloop.netlify.app'],
   credentials: true
 }));
 app.use(express.json()); // Parse JSON request bodies
